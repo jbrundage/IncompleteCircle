594df73c9ba6e5742b71dbc13d0c001e900241f8
Rey <reyneraccel@gmail.com>
2018-10-01 20:07:03 -0300

    Update incomplete.js

--- incomplete.js
+++ incomplete.js
@@ -1,27 +1,39 @@
 import { Circle } from 'circle.js';
 
 export class IncompleteCircle {
-    constructor(start, end){
+    constructor(start, end, radius){
         this.start = start;
         this.end = end;
+        this.radius = radius;
     }
-    /**
-     *
-     * Gets all quadrants present in the incomplete circle
-     *
-     */
-     
+    
+   /**
+    *
+    * Gets all quadrants present in the incomplete circle
+    *
+    */
     allQuadrants () {
         const startQuad = Circle.getQuadrant(this.start) || 1; // coerces 0 to 1st quadrant
         const endQuad = Circle.getQuadrant(this.end) || 1; // corces 0 to 1st quadrant
         let quadrants = [startQuad];
-        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )){ return quadrants; }
+        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )) return quadrants;
         let i = ( startQuad + 1 );
-        while((i % 5) != endQuad){
-            if(i % 5 != 0) quadrants.push(i % 5);
+        while((i % 5) !== endQuad){
+            if(i % 5 !== 0) quadrants.push(i % 5);
             i++;
         }
         quadrants.push(endQuad);
         return quadrants;
+    }
+    
+    getDimensions () {
+        if(this.start === this.end) return null;
+        const quadrants = this.allQuadrants();
+        if(Math.abs(this.end - this.start) === 90){
+            const originS = this.start % 90;
+            const originE = this.end % 90;
+            const untranslated = [this.radius * ( Math.cos(Circle.getRadians(originS)) + Math.cos(Circle.getRadians(OriginE))), this.radius];
+            //incomplete
+        }
     }
 }
