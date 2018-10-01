5fb3aa66710aed72f2430240fbd232072a099060
Rey <reyneraccel@gmail.com>
2018-10-01 18:13:28 -0300

    Update incomplete.js

--- incomplete.js
+++ incomplete.js
@@ -14,19 +14,14 @@ export class IncompleteCircle {
     allQuadrants () {
         const startQuad = Circle.getQuadrant(this.start) || 1; // coerces 0 to 1st quadrant
         const endQuad = Circle.getQuadrant(this.end) || 1; // corces 0 to 1st quadrant
-        let diff = Math.abs(startQuad - endQuad); // gets the number of quadrants between end and start
-        if(diff){ // if difference is higher than zero, calculates interpolated quadrants
-            let quadrants = [startQuad];
-            let i = startQuad;
-            while(diff){
-                quadrants.push(i % 4);
-                diff--;
-                i++;
-            }
-            quadrants.push(endQuad);
-            return quadrants;
-        }else{ // else return initial and ending quadrants
-            return [startQuad, endQuad];
+        let quadrants = [startQuad];
+        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )){ return quadrants; }
+        let i = ( startQuad + 1 );
+        while((i % 5) != endQuad){
+            if(i % 5 != 0) quadrants.push(i % 5);
+            i++;
         }
+        quadrants.push(endQuad);
+        return quadrants;
     }
 }
