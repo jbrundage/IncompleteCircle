0e60f79e6668678e77325499135792baaa378f7a
Rey <reyneraccel@gmail.com>
2018-10-01 20:10:45 -0300

    Update circle.js

--- circle.js
+++ circle.js
@@ -36,7 +36,7 @@ export class Circle {
     *   Transform degrees into radians
     *
     */
-    getRadians = function (degrees) {
+    getRadians (degrees) {
         return degrees * Math.PI / 180;
     }
 }
