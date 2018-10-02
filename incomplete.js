2fe0b75dd511a9fe8ba5bcd5bc31b08034ba480e
Jaman Brundage <jbrundage372@gmail.com>
2018-10-02 14:54:12 -0400

    Completed getDimensions

--- incomplete.js
+++ incomplete.js
@@ -5,6 +5,12 @@ export class IncompleteCircle {
         this.start = start;
         this.end = end;
         this.radius = radius;
+        this.startX = radius * Math.cos(Circle.getRadians(start));
+        this.startY = radius * Math.sin(Circle.getRadians(start));
+        this.endX = radius * Math.cos(Circle.getRadians(end));
+        this.endY = radius * Math.sin(Circle.getRadians(end));
+        this.startQuad = Circle.getQuadrant(this.start);
+        this.endQuad = Circle.getQuadrant(this.end);
     }
     
    /**
@@ -13,38 +19,54 @@ export class IncompleteCircle {
     *
     */
     allQuadrants () {
-        const startQuad = Circle.getQuadrant(this.start);
-        const endQuad = Circle.getQuadrant(this.end);
-        let quadrants = [startQuad];
-        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( startQuad === endQuad )) return quadrants;
-        let i = ( startQuad + 1 );
-        while((i % 5) !== endQuad){
+        let quadrants = [this.startQuad];
+        if(( Circle.getNatural(this.start) <= Circle.getNatural(this.end) ) && ( this.startQuad === this.endQuad )) return quadrants;
+        let i = ( this.startQuad + 1 );
+        while((i % 5) !== this.endQuad){
             if(i % 5 !== 0) quadrants.push(i % 5);
             i++;
         }
-        quadrants.push(endQuad);
+        quadrants.push(this.endQuad);
         return quadrants;
     }
     
     getDimensions () {
         if(this.start === this.end) return null;
         const quadrants = this.allQuadrants();
+        const quadrantsMap = {};
+        quadrants.forEach(quadrant=>quadrantsMap[quadrant]=1);
         const quantity = quadrants.length;
-        if(quantity === 1){
-            
-        }
-        if(quantity === 2){
-        }
-        if(quantity === 3){
-        }
         if(quantity === 4){
             return [2 * this.radius, 2 * this.radius];
         }
-        if(Math.abs(this.end - this.start) === 90){
-            const originS = this.start % 90;
-            const originE = this.end % 90;
-            const untranslated = [this.radius * ( Math.cos(Circle.getRadians(originS)) + Math.cos(Circle.getRadians(OriginE))), this.radius];
-            //incomplete
+        let minX = this.endX > this.startX ? this.startX : this.endX;
+        let minY = this.endY > this.startY ? this.startY : this.endY;
+        let maxX = this.endX < this.startX ? this.startX : this.endX;
+        let maxY = this.endY < this.startY ? this.startY : this.endY;
+        minX = this.radius < minX ? this.radius : minX;
+        minY = this.radius < minY ? this.radius : minY;
+        maxX = this.radius > maxX ? this.radius : maxX;
+        maxY = this.radius > maxY ? this.radius : maxY;
+        if(quadrantsMap[1] && quadrantsMap[2]) {
+            if(this.startQuad === 3 || this.startQuad === 4 || this.endQuad === 3 || this.endQuad === 4){
+                minY = 0;
+            }
         }
+        if(quadrantsMap[2] && quadrantsMap[3]) {
+            if(this.startQuad === 1 || this.startQuad === 4 || this.endQuad === 1 || this.endQuad === 4){
+                minX = 0;
+            }
+        }
+        if(quadrantsMap[3] && quadrantsMap[4]) {
+            if(this.startQuad === 1 || this.startQuad === 2 || this.endQuad === 1 || this.endQuad === 2){
+                maxY = this.radius*2;
+            }
+        }
+        if(quadrantsMap[1] && quadrantsMap[4]) {
+            if(this.startQuad === 1 || this.startQuad === 2 || this.endQuad === 1 || this.endQuad === 2){
+                maxX = this.radius*2;
+            }
+        }
+        return [maxX - minX, maxY - minY];
     }
 }
